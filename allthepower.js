1a587c3383c6bd41b0816e5f587ec7317d54a9a3
tenaciousOne <37675561+tenaciousOne@users.noreply.github.com>
2018-05-12 14:13:53 -0600

    Add files via upload

--- allthepower.js
+++ allthepower.js
@@ -1,26 +1,19 @@
 
 
+var toggleButton = function (play) {
+    var icon = document.getElementById("youtube-icon");
+    var img = play ? "stop.png" : "play.png";
+    icon.setAttribute("src", img);
+}
 
 //create play and pause buttons for audio player
 function onYouTubeIframeAPIReady() {
 
-    var ctrlq = document.getElementById("youtube-audio");
+    var youtubesound = document.getElementById("youtube-audio");
 
-    var icon = document.createElement("img");
-    icon.setAttribute("id", "youtube-icon");
-    icon.style.cssText = "cursor:pointer;cursor:hand";
-    ctrlq.appendChild(icon);
-
-    var div = document.createElement("div");
-    div.setAttribute("id", "youtube-player");
-    ctrlq.appendChild(div);
-
-    var toggleButton = function (play) {
-        var img = play ? "IDzX9gL.png" : "quyUPXN.pgn";
-        icon.setAttribute("src", "https://i.imgur.com/" + img);
-    }
+    
 //these guys above and below toggle between pause and play states on button click
-    cntrl.onclick = function () {
+    youtubesound.onclick = function () {
         if ( player.getPlayerState() === YT.PlayerState.PLAYING
             || player.getPlayerState() === YT.PlayerState.BUFFERING ) {
             player.pauseVideo();
@@ -34,10 +27,10 @@ function onYouTubeIframeAPIReady() {
     var player = new YT.Player('youtube-player', {
         height: '0',
         width: '0',
-        videoId: ctrlq.dataset.video,
+        videoId: youtubesound.dataset.video,
         playerVars: {
-            autoplay: ctrlq.dataset.autoplay,
-            loop: ctrlq.dataset.loop,
+            autoplay: youtubesound.dataset.autoplay,
+            loop: youtubesound.dataset.loop,
         },
         events: {
             'onReady': function(e) {
